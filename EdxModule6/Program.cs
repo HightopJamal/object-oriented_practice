dcacdb5343af0759b822eaa8b94fb4efcdcd6245
HightopJamal <amirjdaniel@gmail.com>
2017-07-07 20:25:55 -0400

    c# practice

---
+++ EdxModule6/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdxModule6
+{   abstract class Person
+    {
+        private string firstname;
+        private string lastname;
+        private string streetaddress;
+        private int zip;
+        private string city;
+
+        public string First
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value; 
+            }
+        }
+
+        public string Last
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+
+        public string 
+
+        public Person(string firstname, string lastname, string streetaddress, int zip, string city)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.streetaddress = streetaddress;
+            this.city = city;
+            this.zip = zip;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
